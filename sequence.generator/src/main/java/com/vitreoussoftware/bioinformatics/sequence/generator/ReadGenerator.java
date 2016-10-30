443384d63c3e80613dad78aee863739e46f48983
John Battista <jmbattista@gmail.com>
2016-10-29 21:52:32 -0700

    Switch ReadGenerator to be a ConditionalDistribution added more tests

--- sequence.generator/src/main/java/com/vitreoussoftware/bioinformatics/sequence/generator/ReadGenerator.java
+++ sequence.generator/src/main/java/com/vitreoussoftware/bioinformatics/sequence/generator/ReadGenerator.java
@@ -3,6 +3,7 @@ package com.vitreoussoftware.bioinformatics.sequence.generator;
 import com.vitreoussoftware.bioinformatics.sequence.BasePair;
 import com.vitreoussoftware.bioinformatics.sequence.Sequence;
 import com.vitreoussoftware.bioinformatics.sequence.basic.BasicSequence;
+import com.vitreoussoftware.bioinformatics.sequence.generator.distribution.ConditionalDistribution;
 import com.vitreoussoftware.bioinformatics.sequence.generator.distribution.Distribution;
 import com.vitreoussoftware.bioinformatics.sequence.generator.distribution.discrete.UniformDistribution;
 import lombok.Builder;
@@ -16,16 +17,11 @@ import java.util.LinkedList;
  *
  * Created by John on 10/25/2016.
  */
-public class ReadGenerator implements Distribution<Sequence> {
+public class ReadGenerator implements ConditionalDistribution<Sequence, Sequence> {
     /**
      * The maximum number of times we will try to generate a valid sample from the provided configuration before erroring out.
      */
     private final static int MAX_RETRIES = 1000;
-
-    /**
-     * The {@link Sequence} to sample reads from
-     */
-    private final Sequence source;
 
     /**
      * This {@link Distribution} is sampled to determine the length of the read
@@ -41,22 +37,20 @@ public class ReadGenerator implements Distribution<Sequence> {
 
     @Builder
     private ReadGenerator(
-            @NonNull final Sequence source,
             @NonNull final Distribution<Integer> lengthDistribution,
             @NonNull final Distribution<Integer> startPointDistribution) {
-        this.source = source;
         this.lengthDistribution = lengthDistribution;
         this.startPointDistribution = startPointDistribution;
     }
 
     @Override
-    public Sequence sample() {
+    public Sequence sample(Sequence source) {
         int length = 0;
         int startPoint = 0;
         int attempts = 0;
 
         while (attempts < MAX_RETRIES &&
-                (length == 0 || length + startPoint > this.source.length())) {
+                (length == 0 || length + startPoint > source.length())) {
             length = lengthDistribution.sample();
             startPoint = startPointDistribution.sample();
             attempts++;
@@ -68,26 +62,9 @@ public class ReadGenerator implements Distribution<Sequence> {
         val list = new LinkedList<BasePair>();
 
         for (int i = startPoint; i < startPoint + length; i++) {
-            list.add(this.source.get(i));
+            list.add(source.get(i));
         }
 
         return BasicSequence.create(list).get();
-    }
-
-    public static class ReadGeneratorBuilder {
-        private Sequence source;
-        private Distribution<Integer> startPointDistribution;
-
-        public ReadGenerator build() {
-            // If no startPointDistirbution is provided default to a uniform distribution
-            if (this.startPointDistribution == null) {
-                this.startPointDistribution = UniformDistribution.builder()
-                        .lower(0.0)
-                        .upper((double) this.source.length())
-                        .build();
-            }
-
-            return new ReadGenerator(source, lengthDistribution, startPointDistribution);
-        }
     }
 }
--- sequence.generator/src/test/java/com/vitreoussoftware/bioinformatics/sequence/generator/ReadGeneratorTest.java
+++ sequence.generator/src/test/java/com/vitreoussoftware/bioinformatics/sequence/generator/ReadGeneratorTest.java
@@ -2,11 +2,11 @@ package com.vitreoussoftware.bioinformatics.sequence.generator;
 
 import com.vitreoussoftware.bioinformatics.sequence.basic.BasicSequence;
 import com.vitreoussoftware.bioinformatics.sequence.encoding.BasicDnaEncodingScheme;
-import com.vitreoussoftware.bioinformatics.sequence.generator.distribution.discrete.UniformDistribution;
 import lombok.val;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.AnyOf.anyOf;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
@@ -19,20 +19,100 @@ import static org.hamcrest.core.IsNull.nullValue;
 public class ReadGeneratorTest {
 
     /**
-     * Test that we can sample lenght 1 reads from a length 1 sequence
+     * Test that we can sample length 1 reads from a length 1 sequence
      */
     @Test
-    public void testSample_fromSingle() {
+    public void testSample_fromSingleA() {
+        val source = BasicSequence.create("A", BasicDnaEncodingScheme.instance).get();
         val generator = ReadGenerator.builder()
-                .source(BasicSequence.create("A", BasicDnaEncodingScheme.instance).get())
                 .lengthDistribution(() -> 1)
                 .startPointDistribution(() -> 0)
                 .build();
 
-        val read = generator.sample();
+        val read = generator.sample(source);
 
         assertThat(read, is(not(nullValue())));
         assertThat(read.length(), is(1));
         assertThat(read.get(0).toChar(), is('A'));
+    }
+
+    /**
+     * Test that we can sample length 1 reads from a length 1 sequence
+     */
+    @Test
+    public void testSample_fromSingleT() {
+        val source = BasicSequence.create("T", BasicDnaEncodingScheme.instance).get();
+        val generator = ReadGenerator.builder()
+                .lengthDistribution(() -> 1)
+                .startPointDistribution(() -> 0)
+                .build();
+
+        val read = generator.sample(source);
+
+        assertThat(read.toString(), is("T"));
+    }
+
+    /**
+     * Test that we can sample repeatedly for a set of valid distributions
+     */
+    @Test
+    public void testSample_repeatInBounds() {
+        val source = BasicSequence.create("TT", BasicDnaEncodingScheme.instance).get();
+        val generator = ReadGenerator.builder()
+                .lengthDistribution(() -> (int)(Math.random() * 2))
+                .startPointDistribution(() -> (int)(Math.random() * 2))
+                .build();
+
+
+        for (int i = 0; i < 100; i++) {
+            val read = generator.sample(source);
+            assertThat(read.toString(), is("T"));
+        }
+    }
+
+    /**
+     * Test that we can sample repeatedly for a set of distributions that can contain invalid values
+     */
+    @Test
+    public void testSample_repeat() {
+        val source = BasicSequence.create("TT", BasicDnaEncodingScheme.instance).get();
+        val generator = ReadGenerator.builder()
+                .lengthDistribution(() -> (int)(Math.random() * 4))
+                .startPointDistribution(() -> (int)(Math.random() * 4))
+                .build();
+
+
+        for (int i = 0; i < 100; i++) {
+            val read = generator.sample(source);
+            assertThat(read.toString(), anyOf(is("T"), is("TT")));
+        }
+    }
+
+    /**
+     * Test that we can fail when attempting to sample from invalid distributions
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testSample_failsInvalidLength() {
+        val source = BasicSequence.create("TT", BasicDnaEncodingScheme.instance).get();
+        val generator = ReadGenerator.builder()
+                .lengthDistribution(() -> 0)
+                .startPointDistribution(() -> 1)
+                .build();
+
+        generator.sample(source);
+    }
+
+    /**
+     * Test that we can fail when attempting to sample from invalid distributions
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testSample_failsInvalidStartPoint() {
+        val source = BasicSequence.create("TT", BasicDnaEncodingScheme.instance).get();
+        val generator = ReadGenerator.builder()
+                .lengthDistribution(() -> 1)
+                .startPointDistribution(() -> 5)
+                .build();
+
+        generator.sample(source);
     }
 }
