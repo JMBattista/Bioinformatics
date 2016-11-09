4da7ae7a11ecc9638dcc24ae7c61fc28f92cbfb1
John Battista <jmbattista@gmail.com>
2016-11-08 22:45:16 -0800

    Remove unused scalatest plugin references

--- scalatestspec/project/plugins/Plugins.scala
+++ scalatestspec/project/plugins/Plugins.scala
@@ -18,10 +18,5 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  
-  lazy val fusesource_snapshot_repo = "FuseSource Snapshots" at
-           "http://repo.fusesource.com/nexus/content/repositories/snapshots"
-
-  lazy val scalate_plugin = "org.fusesource.scalate" % "sbt-scalate-plugin" % "1.6.1"
 
 }
